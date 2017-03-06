7192494093c4e77507c6e99f85b538e8b34b727f
rishit <rishit@RISHIT-PC>
2017-03-06 23:41:37 +0530

    NSEIndices code added

--- StockApplication/NSEindices.vb
+++ StockApplication/NSEindices.vb
@@ -8,15 +8,39 @@ Public Class NSEindices
     Public Function getIndicesListAndStore() As Boolean
         Dim rawIndicesData As String
 
-        rawIndicesData = Helper.GetDataFromUrl("https://www.nseindia.com/homepage/Indices1.json")
 
+        rawIndicesData = Helper.GetDataFromUrl("https://www.nseindia.com/homepage/Indices1.json")
+        parseAndPopulateObjects(rawIndicesData)
         Return True
     End Function
 
     Private Function parseAndPopulateObjects(ByVal rawIndicesData As String) As List(Of NSEindices)
         Dim NSEindicesList As List(Of NSEindices) = New List(Of NSEindices)
-
+        Dim tmpRawIndicesData As String
+        Dim indexOfVar As Integer
         Dim NSEIndicesData As NSEindices
+        Dim countOfSymbols As Integer
+
+        tmpRawIndicesData = rawIndicesData
+        countOfSymbols = rawIndicesData.Split("{""name").Length - 2
+        For count = 1 To countOfSymbols
+            NSEIndicesData = New NSEindices
+            indexOfVar = tmpRawIndicesData.IndexOf("name")
+            tmpRawIndicesData = tmpRawIndicesData.Substring(indexOfVar + 7)
+            NSEIndicesData.indexName = tmpRawIndicesData.Substring(0, tmpRawIndicesData.IndexOf(""","))
+            indexOfVar = tmpRawIndicesData.IndexOf("lastPrice")
+            tmpRawIndicesData = tmpRawIndicesData.Substring(indexOfVar + 12)
+            NSEIndicesData.lastPrice = tmpRawIndicesData.Substring(0, tmpRawIndicesData.IndexOf(""","))
+
+            indexOfVar = tmpRawIndicesData.IndexOf("change")
+            tmpRawIndicesData = tmpRawIndicesData.Substring(indexOfVar + 9)
+            NSEIndicesData.priceChange = tmpRawIndicesData.Substring(0, tmpRawIndicesData.IndexOf(""","))
+
+            indexOfVar = tmpRawIndicesData.IndexOf("pChange")
+            tmpRawIndicesData = tmpRawIndicesData.Substring(indexOfVar + 10)
+            NSEIndicesData.percentageChange = tmpRawIndicesData.Substring(0, tmpRawIndicesData.IndexOf(""","))
+            NSEindicesList.Add(NSEIndicesData)
+        Next count
 
         Return NSEindicesList
     End Function
---
+++ StockApplication/TestForm.vb
@@ -0,0 +1,7 @@
+﻿Public Class TestForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tmpNSEindices As NSEindices
+        tmpNSEindices = New NSEindices()
+        tmpNSEindices.getIndicesListAndStore()
+    End Sub
+End Class
